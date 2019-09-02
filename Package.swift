325b77cc1d0816691a72c58a1a81ede411664aec
manuelvrhovac <manuel.vrhovac@icloud.com>
2019-09-02 08:57:39 +0200

    SPM Support - added Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:4.2
+
+import PackageDescription
+
+let package = Package(
+    name: "KVFetcher",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "KVFetcher",
+            targets: ["KVFetcher"]
+        )
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "KVFetcher",
+            dependencies: []
+        )
+    ]
+)
